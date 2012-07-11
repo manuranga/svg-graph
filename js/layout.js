7089fdd0f7b6addafea1bee22b0f0939b773d17b
Manuranga Perera <rcmperera@gmail.com>
2012-07-12 01:06:57 +0530

    adding new layout file

---
+++ js/layout.js
@@ -0,0 +1,13 @@
+var pigvis = {};
+pigvis.layout = {};
+pigvis.layout.flow = function () {
+    var hGap = 0.1,
+        size = [1, 1]; // width, height
+
+    var flow = {};
+    flow.nodes = function (d) {
+
+    };
+
+    return flow;
+};
